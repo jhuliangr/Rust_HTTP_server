922576cf2920cabbb21356f20a277ac30ca95a18
Jhulian Garcia <jhpiano9731@gmail.com>
2023-10-19 10:46:28 -0400

    making the server a thread, and other minor fixes

--- src/main.rs
+++ src/main.rs
@@ -13,13 +13,11 @@ mod website_handler;
 fn main() {
     let default_path = format!("{}/public", env!("CARGO_MANIFEST_DIR"));
     let public_path = env::var("PUBLIC_PATH").unwrap_or(default_path);
-    // println!("-------------> {}", public_path);
     let server: Server = Server::new("127.0.0.1".to_owned(), 8080);
     let pid = thread::spawn(||server.run(WebsiteHandler::new(public_path)));
     
     pid.join().unwrap();
-    // server.run(WebsiteHandler::new(public_path));
 }
 
-//add threads with threads and sync library
+//add threads and sync library
 //add async work
